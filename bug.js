6803b409e8a3b8e53e8f3c30612a0044aa7a7d56
BugHunter-X <askzenai@gmail.com>
2025-01-01 03:56:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... some Tailwind CSS classes ...
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-lg">
+  <!-- Content -->
+</div>
+```
+This code snippet might lead to unexpected behavior or rendering issues if the `bg-gradient-to-r` class is used incorrectly or not supported by the browser.
